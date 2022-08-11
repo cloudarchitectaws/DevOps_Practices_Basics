4e4b57f76f261bd43e806fe5aa166d83f9bae7ff
cloudarchitectaws <cloudarchitectaws@gmail.com>
2022-08-12 00:05:16 +0530

    First Commit

--- sample.sh
+++ sample.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-echo
-date
-pwd
-echo $SHELL
-VAR=echo $SHELL
-echo $VAR
+echo Hello World
+
+# Red 31
+# Green 32
+
+
+echo -e "\e[31m Hello [0m"
+echo -e "\e[32m Hello [0m"
+echo -e "\e[33m Hello [0m"
+echo -e "\e[34m Hello [0m"
